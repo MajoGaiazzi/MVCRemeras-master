3c56dee372f41cf31bbbbc50e6964dc1b09c51e5
MajoGaiazzi <mariajosegaiazzi@gmail.com>
2022-06-16 02:26:47 -0300

    Subido

---
+++ MVCRemeras-master/Data/MVCMerchandisingContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MVCMerchandising.Models;
+
+namespace MVCMerchandising.Data
+{
+    public class MVCMerchandisingContext : DbContext
+    {
+        public MVCMerchandisingContext (DbContextOptions<MVCMerchandisingContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MVCMerchandising.Models.Merchandising> Merchandising { get; set; }
+    }
+}
---
+++ MVCRemeras-master/Models/Merchandising.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace MVCMerchandising.Models
+{
+    public class Merchandising
+    {
+        public int Id { get; set; }
+
+
+        [StringLength(60, MinimumLength = 3)]
+
+        [Required]
+        public string Producto { get; set; }
+
+        public string Talles { get; set; }
+
+        public string Descripcion { get; set; }
+
+
+
+        [Range(1, 100)]
+
+        [DataType(DataType.Currency)]
+
+        /*[Column(TypeName = "decimal(18, 2)")]*/
+        public decimal Precio { get; set; }
+
+        public string Colores { get; set; }
+
+        public string Imagen { get; set; }
+
+    }
+}
